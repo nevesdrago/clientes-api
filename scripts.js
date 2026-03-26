8ad903843d0b637886af486f86c3546516e8a24d
Eduardo Drago <eduardondrago05@gmail.com>
2026-03-26 18:50:39 -0300

    feature listar usuario

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+// LISTAR USUÁRIO
+document.addEventListener("DOMContentLoaded", () =>  {
+    // Fetch no endpoint
+    fetch("https://crudcrud.com/api/9843cb2768ee4eb6bb727a584faf9d39/clientes")
+    .then(response => response.json())
+    .then(data => {
+        // Receber dados 
+        console.log(data);
+            // Cria o elemento pai ul
+            const clientesUl = document.createElement("ul");
+            data.forEach(item => {
+                // Cria a lista com nome e email
+                const li = document.createElement("li");
+                li.textContent = `Nome: ${item.nome} - Email: ${item.email}`
+                // Adiciona a lista ao elemento pai
+                clientesUl.appendChild(li);
+            });
+            // Adiciona o elemento pai com a lista ao HTML
+            document.getElementById("listarClientes").appendChild(clientesUl);
+
+    })
+    .catch(error => {
+        console.error("Erro:" , error);
+    })
+})
+
+
+
 // CADASTRAR USUÁRIO
 function cadastrarUsuario() {
     // 2 Inputs necessários: Nome e Email
@@ -7,7 +35,7 @@ function cadastrarUsuario() {
 
     // Interagir com a API usando fetch
     // Salvar as infos na API usando post
-    fetch("https://crudcrud.com/api/80c065fdf6f44ac1803bb43804c12b17/clientes", {
+    fetch("https://crudcrud.com/api/9843cb2768ee4eb6bb727a584faf9d39/clientes", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
